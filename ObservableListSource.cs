c9f6e1c26f1112fe33ed2a67b0bf805281a7478d
VasylHudyma <vasia08071998@gmail.com>
2019-11-20 21:47:45 +0200

    init

---
+++ ObservableListSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class ObservableListSource<T> : ObservableCollection<T>, IListSource
+             where T : class
+    {
+        private IBindingList _bindingList;
+
+        bool IListSource.ContainsListCollection { get { return false; } }
+
+        public IList GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        IList IListSource.GetList()
+        {
+            return _bindingList ?? (_bindingList = this.ToBindingList());
+        }
+    }
+}
